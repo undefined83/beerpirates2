123896ee58de0860061aa1b6cfadf98d936de787
Aleksandar Grujic Djajic <grujic.djajic@gmail.com>
2022-06-07 21:32:59 +0200

    calls api doesnt display data

--- api/GetBeerRecommendations.cs
+++ api/GetBeerRecommendations.cs
@@ -16,7 +16,7 @@ namespace Beerpirates.Functions
     {
         [FunctionName("GetBeerRecommendations")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "beerrecommendations")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
